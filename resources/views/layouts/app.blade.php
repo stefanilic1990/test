3973414bc5c2b9626679b2220287c4e45c017889
stefanilic1990 <stefan.ilic1990@outlook.com>
2020-12-01 21:53:36 +0100

    Prelazak na https

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ seasset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
